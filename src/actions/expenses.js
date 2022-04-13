cdb00f272bf8c44bfd17e7e785053e1fa2eb170d
iamhrugved <hrugved.pawar@sjsu.edu>
2022-04-12 23:21:54 -0700

    get firebase expenses on start

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,19 +1,19 @@
-import {v1 as uuid} from 'uuid'
+import { v1 as uuid } from 'uuid'
 import database from '../firebase/firebase'
 
 // ADD_EXPENSE
 export const addExpense = (expense) => ({
     type: 'ADD_EXPENSE',
     expense
-    }
+}
 )
 
 export const startAddExpense = (expenseData = {}) => {
     return (dispatch) => {
         const {
-            description = '', 
-            note = '', 
-            amount = 0, 
+            description = '',
+            note = '',
+            amount = 0,
             createdAt = 0
         } = expenseData;
         const expense = { description, note, amount, createdAt }
@@ -38,3 +38,27 @@ export const editExpense = (id, updates) => ({
     id,
     updates
 })
+
+//SET_EXPENSES
+export const setExpenses = (expenses) => ({
+    type: 'SET_EXPENSES',
+        expenses
+})
+
+
+export const startSetExpenses = () => {
+    return (dispatch) => {
+      return database.ref('expenses').once('value').then((snapshot) => {
+        const expenses = [];
+  
+        snapshot.forEach((childSnapshot) => {
+          expenses.push({
+            id: childSnapshot.key,
+            ...childSnapshot.val()
+          });
+        });
+        console.log(expenses)
+        dispatch(setExpenses(expenses));
+      });
+    };
+  };
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,18 +1,20 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from '@reduxjs/toolkit'
 import expensesReducer from '../reducers/expenses'
 import filtersReducer from '../reducers/filters'
-import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunkMiddleware from 'redux-thunk'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
+
+const rootReducer = 
+    combineReducers({
+    expenses: expensesReducer,
+    filters: filtersReducer
+})
 
 // Store creation
 export default () => {
-    const store = createStore(
-        combineReducers({
-            expenses: expensesReducer,
-            filters: filtersReducer
-        }),
-        composeEnhancers(applyMiddleware(thunk))
-    )
+    const store = createStore(rootReducer, composedEnhancer)
     return store
 }
